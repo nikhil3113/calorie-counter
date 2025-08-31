4cb37aed44a5c5c4875e7f5adbdf597ea942b312
nikhil3113 <chavann717@gmail.com>
2025-08-31 20:34:34 +0530

    Fixed linting and glob error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["app/generated/**", "**/app/generated/**", "**/generated/**"],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
